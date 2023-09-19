95b6d2099134ea93da3b1fb28f7c65bc3b35ee2c
Danny Wittig <wittigdaniel.g@gmail.com>
2023-09-18 21:21:16 -0500

    resume fix

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import download from '../../images/download.svg'
-import resume from '../../images/Resume-Screenshot.png'
+import download from '../../images/download.svg';
+import resumeImage from '../../images/Resume-Screenshot.png';
+import resume from '../../assets/Danny-Wittig-Resume.pdf'
+
 
 export default function Resume() {
   return (
     <div>
-      <h1>Download My Resume Here: <span className='float-end'><a className='link-dark' href='/Danny-Wittig-Resume.pdf' target='blank' download><img className='footerIcon' src={download} alt='download icon' height="50px"/>Danny Wittig's Resume</a></span></h1>
-      <img className='img-fluid p-4' src={resume} alt='resume' />
+      <h1>Download My Resume Here: <span className='float-end'><a className='link-dark' href={resume} target='blank' download><img className='footerIcon' src={download} alt='download icon' height="50px"/>Danny Wittig's Resume</a></span></h1>
+      <img className='img-fluid p-4' src={resumeImage} alt='resume' />
     </div>
   );
 }
